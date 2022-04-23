de56e6f63200ebaf0da307400d0b67cbe49cb3c6
itamar27 <58764132+itamar27@users.noreply.github.com>
2022-04-23 21:42:41 +0300

    Edited research urls

--- research/urls.py
+++ research/urls.py
@@ -3,9 +3,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from research import views
 
 urlpatterns = [
-    path('<int:researchId>/participants/', views.ParticipantList.as_view(), name="participants"),
-    path('<int:researchId>/participants/<int:pk>', views.ParticipantDetails.as_view(), name="participant"),
-    path('<int:researchId>/interactions/', views.InteractionList.as_view(), name="interaction"),
-    path('<int:researchId>/interactions/network',views.InteractionsNetworkAPIView.as_view(), name="interaction network view"),
-    path('<int:researchId>/gameConfiguration', views.GameConfigurationDetail.as_view(), name='game configuration'),
+    path('participants/', views.ParticipantList.as_view(), name="participants"),
+    path('participants/<int:pk>', views.ParticipantDetails.as_view(), name="participant"),
+    path('interactions/', views.InteractionList.as_view(), name="interaction"),
+    path('interactions/network',views.InteractionsNetworkAPIView.as_view(), name="interaction network view"),
+    path('gameConfiguration', views.GameConfigurationDetail.as_view(), name='game configuration'),
 ]
